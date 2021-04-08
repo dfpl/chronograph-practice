410b22fb6a908f068f5ff2b40be3681cb281f3b7
JaewookByun <bjw0829@gmail.com>
2021-04-08 16:41:21 +0900

    V(), V(String, Object), E(), E(String, Object), has(String), has(String, Object), has(String, NC, Object), toList are implemented

--- chronograph/src/main/java/com/tinkerpop/gremlin/GremlinFluentPipeline.java
+++ chronograph/src/main/java/com/tinkerpop/gremlin/GremlinFluentPipeline.java
@@ -2,8 +2,10 @@ package com.tinkerpop.gremlin;
 
 import java.util.List;
 
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.common.Tokens;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -13,12 +15,116 @@ import com.tinkerpop.blueprints.Vertex;
 public interface GremlinFluentPipeline<S, E> {
 
 	/**
-	 * Add a GraphQueryPipe to the end of the Pipeline. If optimizations are
-	 * enabled, then the the next steps can fold into a GraphQueryPipe compilation.
+	 * Move traverser from Graph to all the vertices
+	 * 
+	 * Type: transform
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
 	 *
 	 * @return the extended Pipeline
 	 */
 	public GremlinFluentPipeline<Graph, Vertex> V();
 
+	/**
+	 * Move traverser from Graph to all the vertices matched with key-value
+	 *
+	 * Type: transform
+	 *
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 *
+	 * @param key   they key that all the emitted vertices should be checked on
+	 * @param value the value that all the emitted vertices should have for the key
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<Graph, Vertex> V(String key, Object value);
+
+	/**
+	 * Move traverser from Graph to all the edges
+	 * 
+	 * Type: transform
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 * 
+	 *
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<Graph, Edge> E();
+
+	/**
+	 * Move traverser from Graph to all the edges matched with key-value
+	 * 
+	 * Type: transform
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 * 
+	 * @param key   they key that all the emitted edges should be checked on
+	 * @param value the value that all the emitted edges should have for the key
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<Graph, Edge> E(String key, Object value);
+
+	/**
+	 * Check if the vertex or edge has a property with provided key.
+	 *
+	 * Type: filter
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 *
+	 * @param key the property key to check
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<S, S> has(String key);
+
+	/**
+	 * Check if the vertex or edge has a property with provided key-value.
+	 *
+	 * Type: filter
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 *
+	 * @param key   the property key to check
+	 * @param value the object to filter on (in an OR manner)
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<S, S> has(String key, Object value);
+
+	/**
+	 * Check if the vertex or edge has a property with provided key-predicate-value.
+	 *
+	 * Type: filter
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: false
+	 *
+	 * @param key          the property key to check
+	 * @param compareToken the comparison to use
+	 * @param value        the object to filter on
+	 * @return the extended Pipeline
+	 */
+	public GremlinFluentPipeline<S, S> has(String key, Tokens.NC compareToken, Object value);
+
+	/**
+	 * 
+	 * Collect the current traversers in Gremlin as List
+	 * 
+	 * Lazy Evaluation: true
+	 * 
+	 * Terminal Step: true
+	 * 
+	 * @return the current traversers in Gremlin as List
+	 */
 	public List<E> toList();
 }
--- chronograph/src/main/java/org/dfpl/chronograph/traversal/memory/jgremlin/JTraversalEngine.java
+++ chronograph/src/main/java/org/dfpl/chronograph/traversal/memory/jgremlin/JTraversalEngine.java
@@ -3,8 +3,10 @@ package org.dfpl.chronograph.traversal.memory.jgremlin;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.common.Tokens.NC;
 import com.tinkerpop.gremlin.GremlinFluentPipeline;
 import com.tinkerpop.gremlin.GremlinPipeline;
 
@@ -17,16 +19,172 @@ public class JTraversalEngine<S, E> extends GremlinPipeline<S, E> implements Gre
 
 	@Override
 	public GremlinFluentPipeline<Graph, Vertex> V() {
+		// Check the type of input
+		checkInputElementClass(Graph.class);
 
+		// Modify stream
 		stream = stream.flatMap(g -> ((Graph) g).getVertices().stream());
+
+		// Set the class of element
 		elementClass = Vertex.class;
 
+		// return the extended stream
 		return (GremlinFluentPipeline<Graph, Vertex>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<Graph, Vertex> V(String key, Object value) {
+		// Check the type of input
+		checkInputElementClass(Graph.class);
+
+		// Modify stream
+		stream = stream.flatMap(g -> ((Graph) g).getVertices(key, value).stream());
+
+		// Set the class of element
+		elementClass = Vertex.class;
+
+		// return the extended stream
+		return (GremlinFluentPipeline<Graph, Vertex>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<Graph, Edge> E() {
+		// Check the type of input
+		checkInputElementClass(Graph.class);
+
+		// Modify stream
+		stream = stream.flatMap(g -> ((Graph) g).getEdges().stream());
+
+		// Set the class of element
+		elementClass = Edge.class;
+
+		// return the extended stream
+		return (GremlinFluentPipeline<Graph, Edge>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<Graph, Edge> E(String key, Object value) {
+		// Check the type of input
+		checkInputElementClass(Graph.class);
+
+		// Modify stream
+		stream = stream.flatMap(g -> ((Graph) g).getEdges(key, value).stream());
+
+		// Set the class of element
+		elementClass = Edge.class;
+
+		// return the extended stream
+		return (GremlinFluentPipeline<Graph, Edge>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<S, S> has(String key) {
+		// Check the type of input
+		checkInputElementClass(Vertex.class, Edge.class);
+
+		// Modify stream
+		stream = stream.filter(element -> {
+			if (element instanceof Vertex) {
+				return ((Vertex) element).getPropertyKeys().contains(key);
+			} else {
+				return ((Edge) element).getPropertyKeys().contains(key);
+			}
+		});
+
+		// Set the class of element: no change of element class
+
+		// return the extended stream
+		return (GremlinFluentPipeline<S, S>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<S, S> has(String key, Object value) {
+		// Check the type of input
+		checkInputElementClass(Vertex.class, Edge.class);
+
+		// Modify stream
+		stream = stream.filter(element -> {
+			if (element instanceof Vertex) {
+				Object val = ((Vertex) element).getProperty(key);
+				if (val != null && val.equals(value))
+					return true;
+				return false;
+			} else {
+				Object val = ((Edge) element).getProperty(key);
+				if (val != null && val.equals(value))
+					return true;
+				return false;
+			}
+		});
+
+		// Set the class of element: no change of element class
+
+		// return the extended stream
+		return (GremlinFluentPipeline<S, S>) this;
+	}
+
+	@Override
+	public GremlinFluentPipeline<S, S> has(String key, NC t, Object value) {
+		// Check the type of input
+		checkInputElementClass(Vertex.class, Edge.class);
+
+		// Modify stream
+		stream = stream.filter(element -> {
+
+			Object val = null;
+
+			if (element instanceof Vertex) {
+				val = ((Vertex) element).getProperty(key);
+			} else {
+				val = ((Edge) element).getProperty(key);
+			}
+			val = ((Vertex) element).getProperty(key);
+			if (val == null)
+				return false;
+			try {
+				if (val instanceof Comparable) {
+					int compare = ((Comparable) val).compareTo(value);
+					// Returns anegative integer, zero, or a positive integer
+					// as this object is lessthan, equal to, or greater than the specified object.
+					if (compare == 0 && (t.equals(NC.$eq) || t.equals(NC.$gte) || t.equals(NC.$lte))) {
+						return true;
+					} else if (compare < 0 && (t.equals(NC.$lt) || t.equals(NC.$lte) || t.equals(NC.$ne))) {
+						return true;
+					} else if (compare > 0 && (t.equals(NC.$gt) || t.equals(NC.$gte) || t.equals(NC.$ne))) {
+						return true;
+					}
+					return false;
+				} else {
+					return false;
+				}
+			} catch (RuntimeException e) {
+				return false;
+			}
+		});
+
+		// Set the class of element: no change of element class
+
+		// return the extended stream
+		return (GremlinFluentPipeline<S, S>) this;
 	}
 
 	@Override
 	public List toList() {
 		return (List) stream.collect(Collectors.toList());
+	}
+
+	private void checkInputElementClass(Class... correctClasses) {
+		boolean isMatched = false;
+		for (Class correct : correctClasses) {
+			if (elementClass == correct) {
+				isMatched = true;
+				break;
+			}
+		}
+		if (isMatched == false) {
+			throw new UnsupportedOperationException(
+					"Current stream element class " + elementClass + " should be one of " + correctClasses);
+		}
 	}
 
 }
